fa7856b457a0686bed4747bb39c5fdec2f6d7d1b
Bjornskjald <github@bjorn.ml>
2017-05-17 14:53:49 +0200

    cleaned promises

--- index.js
+++ index.js
@@ -11,20 +11,19 @@ const rp = require('request-promise').defaults({followAllRedirects: true});
 module.exports = {
 	login: (name, pass) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') return {status: 'Nieprawidłowa nazwa użytkownika/hasło'};
+			if(typeof name !== 'string' || typeof pass !== 'string') reject('Nieprawidłowa nazwa użytkownika/hasło');
 			if(data.hasOwnProperty(name)) {
 				if(debug) console.log('User istnieje');
 				if(typeof data[name].jar === 'undefined') {
 					if(debug) console.log('Tworze userowi ciasteczka')
 					data[name].jar = rp.jar();
 				}
-				if(data[name].hasOwnProperty('pass')) {
-					resolve(pass === data[name].pass ? {status: 'success'} : {status: 'Nieprawidłowa nazwa użytkownika/hasło'})
+				if(data[name].hasOwnProperty('pass') && pass === data[name].pass) {
+					resolve()
 				} else {
-					resolve({status: 'Użytkownik nie ma hasła.'})
+					reject('Nieprawidłowa nazwa użytkownika/hasło')
 				}
 			} else {
-				// return {status: 'Nieprawidłowa nazwa użytkownika/hasło'}
 				if(debug) console.log('User nie istnieje');
 				data[name] = {pass: pass};
 				if(typeof data[name].jar === 'undefined') {
@@ -33,9 +32,10 @@ module.exports = {
 				}
 				checkLoggedIn(name, pass).then(result => {
 					if(result.status === 'success'){
-						resolve({status: 'success'})
+						resolve()
 					} else {
 						delete data[name]
+						reject('Nieprawidłowa nazwa użytkownika/hasło')
 					}
 				})
 			}
@@ -43,87 +43,112 @@ module.exports = {
 	},
 	oceny: (name, pass) => {
 		return new Promise((resolve, reject) => {
-			if(debug) console.log('Pobieram oceny...')
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(debug) console.log('Pobieram oceny...');
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				if(debug) console.log('Jest ciastko, pobieram...')
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_panelRodzica/oceny/WS_ocenyUcznia.asmx/pobierzOcenyUcznia',
-					body: {idPozDziennika: data[name].id}, json: true, method: 'POST',
-					jar: data[name].jar, resolveWithFullResponse: true
+					body: {idPozDziennika: data[name].id}, 
+					json: true, method: 'POST',
+					jar: data[name].jar
 				}).then(response => {
-					resolve({status: 'success', data: response.body});
+					resolve(response);
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
 	},
 	uwagi: (name, pass) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_panelRodzica/uwagi/WS_uwagiUcznia.asmx/pobierzUwagiUcznia',
-					body: {idPozDziennika: data[name].id}, json: true, method: 'POST',
-					jar: data[name].jar, resolveWithFullResponse: true
+					body: {idPozDziennika: data[name].id}, 
+					json: true, method: 'POST',
+					jar: data[name].jar
 				}).then(response => {
-					resolve({status: 'success', data: response.body});
+					resolve(response);
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
 	},
 	plan: (name, pass, date) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_panelRodzica/plan/WS_Plan.asmx/pobierzPlanZajec',
-					body: {idPozDziennika: data[name].id, data: date, pidRokSzkolny: 16}, json: true, method: 'POST',
-					jar: data[name].jar, resolveWithFullResponse: true
+					body: {idPozDziennika: data[name].id, data: date, pidRokSzkolny: 16}, 
+					json: true, method: 'POST',
+					jar: data[name].jar
 				}).then(response => {
-					resolve({status: 'success', data: response.body});
+					resolve(response);
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
 	},
 	odbiorcy: (name, pass) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_komunikator/WS_wiadomosci.asmx/pobierzListeOdbiorcow',
 					body: {},
 					json: true, method: 'POST',
-					jar: data[name].jar, resolveWithFullResponse: true
+					jar: data[name].jar
 				}).then(response => {
-					resolve({status: 'success', data: response.body});
+					resolve(response);
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
 	},
 	wiadomosc: (name, pass, id) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_komunikator/WS_wiadomosci.asmx/PobierzWiadomosc',
-					json: true, method: 'POST',
 					body: {idWiadomosci: id, typWiadomosci: 0},
+					json: true, method: 'POST',
 					jar: data[name].jar, resolveWithFullResponse: true
 				}).then(response => {
 					resolve({status: 'success', data: response.body});
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
 	},
 	pracownicyJednostki: (name, pass, idjednostki) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_komunikator/WS_wiadomosci.asmx/pobierzPracownikowDlaWybranejJedn',
@@ -131,25 +156,22 @@ module.exports = {
 					jar: data[name].jar, resolveWithFullResponse: true
 				}).then(response => {
 					resolve({status: 'success', data: response.body});
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
 	},
 	odebrane: (name, pass) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_komunikator/WS_wiadomosci.asmx/PobierzListeWiadomosci ',
 					body: {
-						/*'param': {
-							'parametryFiltrow': [{'idKolumny': 'w.Typ_wiadomosci','paramWartosc': 1}],
-							'iloscNaStrone': 99999,
-							'kolumnaSort': 'Data_nadania',
-							'kierunekSort': 1,
-							'panelFiltrow': 1
-						},*/
 						param: {
 							strona: 1,
 							iloscNaStrone: 30,
@@ -164,31 +186,40 @@ module.exports = {
 					json: true, method: 'POST',
 					jar: data[name].jar, resolveWithFullResponse: true
 				}).then(response => {
-					resolve({status: 'success', data: response.body});
+					resolve(response.body);
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
 	},
 	wyslane: (name, pass) => {
 		return new Promise((resolve, reject) => {
-			if(typeof name !== 'string' || typeof pass !== 'string') resolve({status: 'Nieprawidłowe dane.'});
-			if(!data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass')) resolve({status: 'Zaloguj się.'});
+			if(userExists(name, pass)){
+				reject('Use idziennik.login first.')
+				return
+			}
 			checkLoggedIn(name, pass).then(() => {
 				rp({
 					uri: 'https://iuczniowie.pe.szczecin.pl/mod_komunikator/WS_wiadomosci.asmx/PobierzListeWiadomosci ',
 					body: {
-						'param': {
-							'parametryFiltrow': [{'idKolumny': 'w.Typ_wiadomosci','paramWartosc': 0}],
-							'iloscNaStrone': 99999,
-							'kolumnaSort': 'Data_nadania',
-							'kierunekSort': 1,
-							'panelFiltrow': 1
+						param: {
+							strona: 1,
+							iloscNaStrone: 30,
+							iloscRekordow: -1,
+							kolumnaSort: "Data_nadania",
+							kierunekSort: 1,
+							maxIloscZaznaczonych: 0,
+							panelFiltrow: 1,
+							parametryFiltrow: [{idKolumny:"w.Typ_wiadomosci",paramWartosc:"1"}]
 						}
-					}, 
+					},
 					json: true, method: 'POST',
 					jar: data[name].jar, resolveWithFullResponse: true
 				}).then(response => {
-					resolve({status: 'success', data: response.body});
+					resolve(response.body);
+				}).catch(err => {
+					reject(err);
 				})
 			})
 		})
@@ -314,4 +345,12 @@ function checkLoggedIn(name, pass){
 			}
 		})
 	})
+}
+
+function userExists(name, pass){
+	if(typeof name !== 'string' || typeof pass !== 'string' || !data.hasOwnProperty(name) || !data[name].hasOwnProperty('pass') || !data[name].hasOwnProperty('jar')){
+		return true
+	} else {
+		return false
+	}
 }
