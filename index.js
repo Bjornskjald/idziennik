91c153b4fb4074ae4ef1d44cb11b4a66ef694901
Bjornskjald <github@bjorn.ml>
2017-05-22 13:42:22 +0200

    bugfix

--- index.js
+++ index.js
@@ -348,7 +348,7 @@ function Client(name, jar, id){
 	 * @returns {object} Obiekt z danymi pobranymi z API
 	 */
 	this.praceDomowe = (date) => {
-		date.setHours(d.date.getHours() - d.date.getTimezoneOffset() / 60);
+		date.setHours(date.getHours() - date.getTimezoneOffset() / 60);
 		return new Promise((resolve, reject) => {
 			rp({
 				uri: 'https://iuczniowie.pe.szczecin.pl/mod_panelRodzica/pracaDomowa/WS_pracaDomowa.asmx/pobierzPraceDomowe',
@@ -357,7 +357,7 @@ function Client(name, jar, id){
 						strona: 1,
 						iloscNaStrone: 999,
 						iloscRekordow: -1,
-						kolumnaSort: "DataZadania",
+						kolumnaSort: "DataOddania",
 						kierunekSort: 0,
 						maxIloscZaznaczonych: 0,
 						panelFiltrow: 0,
