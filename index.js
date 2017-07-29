4b424abd6417dd98a2bb77541d0b7159f67c0880
Jan Rosa <gitlab@bjorn.ml>
2017-07-29 17:07:50 +0200

    poprawienie roku szkolnego

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function Client (name, agent, id, hash) {
     return new Promise((resolve, reject) => {
       this.agent
       .post('https://iuczniowie.pe.szczecin.pl/mod_panelRodzica/plan/WS_Plan.asmx/pobierzPlanZajec')
-      .send({idPozDziennika: this.id, data: date, pidRokSzkolny: 16})
+      .send({idPozDziennika: this.id, data: date, pidRokSzkolny: 17})
       .then(response => {
         resolve(JSON.parse(response.text).d)
       })
