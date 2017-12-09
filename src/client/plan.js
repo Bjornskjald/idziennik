b6519b3b54d4f1eb12b7cc9a4d56954592503519
Bjornskjald <github@bjorn.ml>
2017-12-09 13:51:35 +0100

    fixed missing function argument

--- src/client/plan.js
+++ src/client/plan.js
@@ -1,4 +1,4 @@
-module.exports = function () {
+module.exports = function (date) {
   return new Promise((resolve, reject) => {
     this.agent
     .post('https://iuczniowie.pe.szczecin.pl/mod_panelRodzica/plan/WS_Plan.asmx/pobierzPlanZajec')
