9d00482f7a43a2ad74b41e66f91d2e7db1e24ac2
Bjornskjald <github@bjorn.ml>
2017-12-09 14:08:18 +0100

    fixed sending confirmations for sent messages

--- src/client/wiadomosc.js
+++ src/client/wiadomosc.js
@@ -2,7 +2,7 @@ module.exports = function (id) {
   return new Promise((resolve, reject) => {
     this.agent
     .post('https://iuczniowie.pe.szczecin.pl/mod_komunikator/WS_wiadomosci.asmx/PobierzWiadomosc')
-    .send({idWiadomosci: id, typWiadomosci: 0})
+    .send({idWiadomosci: id, typWiadomosci: 1})
     .then(response => {
       resolve(JSON.parse(response.text).d)
     })
